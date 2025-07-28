99285b76b311ff79ecc3501f13f5c3ec741e49bf
Wannylet <josehrzcar.software@gmail.com>
2025-07-28 00:52:11 +0000

    Modificacion del archivo entrada.py para que redirija a index.html con las rutas desconocidas y Vue Router se encargue de redireccionar.

--- backend/entrada.py
+++ backend/entrada.py
@@ -15,11 +15,12 @@ app = FastAPI()
 ruta_dist = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "frontend", "dist"))
 
 # Servir archivos estáticos desde la carpeta frontend
-app.mount("/", StaticFiles(directory=ruta_dist, html=True), name="frontend")
+app.mount("/assets", StaticFiles(directory=os.path.join(ruta_dist, "assets")), name="assets")
 
 # Ruta fallback para Vue Router (todas las rutas no encontradas van a index.html)
 @app.get("/{full_path:path}")
 async def retroceso_vue_router(full_path: str):
-    return FileResponse(os.path.join(ruta_dist, "index.html"))
+    ruta_index = os.path.join(ruta_dist, "index.html")
+    return FileResponse(ruta_index)
 
 app.include_router(router_iniciar_sesion)
